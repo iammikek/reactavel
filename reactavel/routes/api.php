226b615f21be62a48d3e0b52b060940321c42a0d
Mike Karthauser <support@brightstorm.co.uk>
2018-08-14 12:57:58 +0100

    adding routes

--- reactavel/routes/api.php
+++ reactavel/routes/api.php
@@ -13,6 +13,32 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::get('products', function () {
+    return response(\App\Product::all(), 200);
+});
+
+Route::get('products/{product}', function ($productId) {
+    $response = response(\App\Product::findOrFail($productId), 200);
+    return $response;
+});
+
+Route::post('products', function (Request $request) {
+    return \App\Product::create($request->all);
+});
+
+Route::put('products/{product}', function (Request $request, $productId) {
+    $product = \App\Product::findOrFail($productId);
+    $product->update($request->all());
+    return $product;
+});
+
+Route::delete('products/{product}', function ($productId) {
+
+    \App\Product::find($productId)->delete();
+
+    return 204;
 });
